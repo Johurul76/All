5d01e9729eb8f456a89be8b0642e5c6d27d378e7
Md.X <Johurultelicom@gmail.com>
2024-10-26 19:12:23 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showContent(contentType) {
+    const contentView = document.getElementById('content-view');
+    contentView.innerHTML = `<p>${contentType} section content will be shown here.</p>`;
+}
